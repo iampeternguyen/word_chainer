4d2023bd857e61c0378e1598380ab893cf04cae5
iampeternguyen <ptnguyen1987@gmail.com>
2019-04-30 19:27:46 +0800

    find adjacent words method functioning

--- lib/word_chainer.rb
+++ lib/word_chainer.rb
@@ -15,9 +15,24 @@ class WordChainer
     end
     dict
   end
+
+  def adjacent_words(word)
+    alphabet = ('a'..'z').to_a
+    adjacents = []
+    (0...word.length).each do |i|
+      alphabet.each do |letter|
+        new_word = word.dup
+        new_word[i] = letter
+        if @dictionary.include?(new_word) && word != new_word
+          adjacents << new_word
+        end
+      end
+    end
+    adjacents
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   chainer = WordChainer.new
-  p chainer.dictionary
+  p chainer.adjacent_words('cat')
 end
